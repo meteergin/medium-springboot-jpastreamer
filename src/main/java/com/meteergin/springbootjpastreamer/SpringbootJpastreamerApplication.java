5f0ba8435894058a91e5f80a0fa5f9b374482aac
Mete Ergin <mete.ergin@outlook.com>
2021-08-19 20:50:59 +0300

    First commit.

---
+++ src/main/java/com/meteergin/springbootjpastreamer/SpringbootJpastreamerApplication.java
@@ -0,0 +1,13 @@
+package com.meteergin.springbootjpastreamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpastreamerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpastreamerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meteergin/springbootjpastreamer/SpringbootJpastreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meteergin.springbootjpastreamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpastreamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
